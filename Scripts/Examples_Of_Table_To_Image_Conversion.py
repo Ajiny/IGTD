dd7df061b9ab7c461427343b92d2f4003c108297
Ajiny <91933032+Ajiny@users.noreply.github.com>
2023-12-05 01:35:54 -0800

    CVD data conversion success

--- Scripts/Examples_Of_Table_To_Image_Conversion.py
+++ Scripts/Examples_Of_Table_To_Image_Conversion.py
@@ -4,8 +4,8 @@ from IGTD_Functions import min_max_transform, table_to_image, select_features_by
 
 
 # 99 columns
-num_row = 0    # Number of pixel rows in image representation
-num_col = 10    # Number of pixel columns in image representation
+num_row = 4    # Number of pixel rows in image representation
+num_col = 4    # Number of pixel columns in image representation
 num = num_row * num_col # Number of features to be included for analysis, which is also the total number of pixels in image representation
 save_image_size = 3 # Size of pictures (in inches) saved during the execution of IGTD algorithm.
 max_step = 10000    # The maximum number of iterations to run the IGTD algorithm, if it does not converge.
@@ -13,19 +13,25 @@ val_step = 50  # The number of iterations for determining algorithm convergence.
                 # is smaller than a pre-set threshold for val_step itertions, the algorithm converges.
 
 # Import the example data and linearly scale each feature so that its minimum and maximum values are 0 and 1, respectively.
+data = pd.read_csv('../MyData/cardio_train.csv', nrows=2000, index_col=0)
 
-data = pd.read_csv('../MyData/depression.csv', index_col=0)
-#y = df['cardio']
-#X = df.drop(['cardio'], axis=1).copy()
-# just make sure to remove the quotation if the numbers are not string
-positive_data = data[data['PHQ'] == 1]
-positive_data = positive_data.drop(['PHQ', 'Y1MED1FF'], axis=1).copy()
+# adjust age to years
+data['age'] =  data['age'] / 365.24
 
-negative_data = data[data['PHQ'] == 0]
-negative_data = negative_data.drop(['PHQ', 'Y1MED1FF'], axis=1).copy()
+# make your blood pressure values are within range and that ap_hi is higher than ap_lo
+data = data[(data['ap_lo'] <= 370) & (data['ap_lo'] > 0)].reset_index(drop=True)
+data = data[(data['ap_hi'] <= 370) & (data['ap_hi'] > 0)].reset_index(drop=True)
+data = data[data['ap_hi'] >= data['ap_lo']].reset_index(drop=True)
 
-positive_data.to_csv('depression_positive.csv', index=False)
-negative_data.to_csv('depression_negative.csv', index=False)
+# split positive and negative data
+positive_data = data[data['cardio'] == 1]
+positive_data = positive_data.drop(['cardio'], axis=1).copy()
+
+negative_data = data[data['cardio'] == 0]
+negative_data = negative_data.drop(['cardio'], axis=1).copy()
+
+positive_data.to_csv('cvd_positive.csv', index=False)
+negative_data.to_csv('cvd_negative.csv', index=False)
 
 #positive_data.info()
 #negative_data.info()
@@ -52,8 +58,8 @@ fea_dist_method = 'Euclidean'
 image_dist_method = 'Euclidean'
 error = 'abs'
 
-result_dir_p = '../depResults/Table_To_Image_Conversion/depression_positive/Test_1'
-result_dir_n = '../depResults/Table_To_Image_Conversion/depression_negative/Test_1'
+result_dir_p = '../CVDResults/Table_To_Image_Conversion/cvd_positive/Test_1'
+result_dir_n = '../CVDResults/Table_To_Image_Conversion/cvd_negative/Test_1'
 
 os.makedirs(name=result_dir_p, exist_ok=True)
 os.makedirs(name=result_dir_n, exist_ok=True)
@@ -71,8 +77,8 @@ image_dist_method = 'Manhattan'
 error = 'squared'
 norm_positive_data = norm_positive_data.iloc[:, :800]
 norm_negative_data = norm_negative_data.iloc[:, :800]
-result_dir_p = '../depResults/Table_To_Image_Conversion/depression_positive/Test_2'
-result_dir_n = '../depResults/Table_To_Image_Conversion/depression_negative/Test_2'
+result_dir_p = '../CVDResults/Table_To_Image_Conversion/cvd_positive/Test_2'
+result_dir_n = '../CVDResults/Table_To_Image_Conversion/cvd_negative/Test_2'
 
 os.makedirs(name=result_dir_p, exist_ok=True)
 os.makedirs(name=result_dir_n, exist_ok=True)
